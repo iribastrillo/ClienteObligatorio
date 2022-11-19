1030b75a62909978a7499af10482a98a4dc5cd40
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-11-19 19:25:07 -0300

    Se agrega busqueda por ISO

--- Cliente/Controllers/SearchController.cs
+++ Cliente/Controllers/SearchController.cs
@@ -38,9 +38,34 @@ namespace Cliente.Controllers
             }     
         }
 
-        public IActionResult ByIsoOrNationalTeam (string query)
+        public IActionResult ByIso(string query)
         {
-            return View();
+            var client = new RestClient("https://localhost:44348/api/match/byCountryISO/" + query);
+            var request = new RestRequest();
+            request.AddParameter("countryISO", query);
+            request.AddHeader("Content-Type", "application/json");
+
+            try
+            {
+                var response = client.Get(request);
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+
+                if(response.Content != null)
+                {
+                    var matches = JsonSerializer.Deserialize<IEnumerable<MatchViewModel>>(response.Content, options);
+                    return View("Results", matches);
+                }
+                return View("NoResults");
+            }
+            catch
+            {
+                return View("NoResults");
+            } 
+
         }
     }
 }
