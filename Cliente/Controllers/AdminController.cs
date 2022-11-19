4dceeff6248378c652b55ae2325a40b76468ffc2
Sintesis <iribastrillo@gmail.com>
2022-11-19 20:22:33 -0300

    ADD: sigue manejo de errores.

--- Cliente/Controllers/AdminController.cs
+++ Cliente/Controllers/AdminController.cs
@@ -15,15 +15,25 @@ namespace Cliente.Controllers
             var client = new RestClient("https://localhost:44348/api/groupsstage");
             var request = new RestRequest();
             request.AddHeader("Contet-Type", "application/json");
-            var response = client.Get(request);
+            RestResponse response = client.ExecuteGet(request);
 
             JsonSerializerOptions options = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true
             };
+
             var groups = JsonSerializer.Deserialize<IEnumerable<GroupStageViewModel>>(response.Content, options);
             ViewBag.Groups = groups;
+
+            client = new RestClient("https://localhost:44348/api/countries");
+            request = new RestRequest();
+            request.AddHeader("Contet-Type", "application/json");
+            response = client.ExecuteGet(request);
+
+            var countries = JsonSerializer.Deserialize<IEnumerable<CountryViewModel>>(response.Content, options);
+
+            ViewBag.Countries = countries;
 
             return View();
         }
