855960506b7bd6bb3009bf7d3380b257fd22cfa7
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-11-19 19:37:49 -0300

    agrgega busqueda por nombre de seleccion

--- Cliente/Controllers/SearchController.cs
+++ Cliente/Controllers/SearchController.cs
@@ -67,5 +67,33 @@ namespace Cliente.Controllers
             } 
 
         }
+
+        public IActionResult ByNationalTeamName(string query)
+        {
+            var client = new RestClient("https://localhost:44348/api/match/byCountryName/" + query);
+            var request = new RestRequest();
+            request.AddParameter("countryName", query);
+            request.AddHeader("Content-Type", "application/json");
+            try
+            {
+                var response = client.Get(request);
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+
+                if(response.Content != null)
+                {
+                    var matches = JsonSerializer.Deserialize<IEnumerable<MatchViewModel>>(response.Content, options);
+                    return View("Results", matches);
+                }
+                return View("NoResults");
+            } 
+            catch
+            {
+                return View("NoResults");
+            }
+        }
     }
 }
