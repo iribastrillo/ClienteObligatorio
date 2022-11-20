ca3def2b86b29dde37c950dad71af223e122d2a9
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-11-20 18:36:14 -0300

    se hace la vista de tabla x grupo, falta corregir datos, y pantalla de error

--- Cliente/Controllers/GroupStageController.cs
+++ Cliente/Controllers/GroupStageController.cs
@@ -50,5 +50,33 @@ namespace Cliente.Controllers
             GroupStageDTO groupStage = new GroupStageDTO(id, group);
             return View(groupStage);
         }
+        public IActionResult GetTableByBroup(string group)
+        {
+            var client = new RestClient("https://localhost:44348/api/groupsStage/byGroup/" + group);
+            var request = new RestRequest();
+
+            request.AddParameter("groupName", group);
+            request.AddHeader("Content-Type", "application/json");
+
+            try
+            {
+                var response = client.Get(request);
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+                if(response.Content != null)
+                {
+                    var nts = JsonSerializer.Deserialize<IEnumerable<TableGroupNTsViewModel>>(response.Content, options);
+                    return View("ByGroup", nts);
+                }
+                return View("ByGroup");
+            } 
+            catch
+            {
+                return View("ByGroup");
+            }
+        }
     }
 }
---
+++ Cliente/Models/VMs/TableGroupNTsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cliente.Models.VMs
+{
+    public class TableGroupNTsViewModel
+    {
+        public string country { get; set; }
+        public int pts { get; set; }
+        public int goalsScored { get; set; }
+        public int goalsAgainst { get; set; }
+        public int yellowCards { get; set; }
+        public int redCards { get; set; }
+        public int directRedCards { get; set; }
+    }
+}
