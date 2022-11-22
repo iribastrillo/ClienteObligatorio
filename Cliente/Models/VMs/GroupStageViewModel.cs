9d1809d1e3583591f0b63c24c4f90314a3a156fc
Sintesis <iribastrillo@gmail.com>
2022-11-21 21:05:49 -0300

    STYLE: cambia display name de national team VM y group stage DTO.

--- Cliente/Models/VMs/GroupStageViewModel.cs
+++ Cliente/Models/VMs/GroupStageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Cliente.Models.VMs
     public class GroupStageViewModel
     {
         public int id { get; set; }
+        [Display(Name = "Grupo")]
         public string group { get; set; }
         public List<NationalTeamViewModel> nationalTeams { get; set; }
     }
